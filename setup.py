777ced757d0b4f451826707e3c8314f47e8fcdd4
Shuaiwen Wang <sw2853@columbia.edu>
2018-08-27 02:26:22 -0400

    modify setup for compiling cotimization

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-ext_modules = [
+ext_modules_cscalar = [
         Extension("AmpBridge.cscalar.clib", sources=["AmpBridge/cscalar/clib.pyx"], libraries=["m"]),
         Extension("AmpBridge.cscalar.gaussian", sources=["AmpBridge/cscalar/gaussian.pyx"], libraries=["m"]),
         Extension("AmpBridge.cscalar.proximal", sources=["AmpBridge/cscalar/proximal.pyx"], libraries=["m"]),
@@ -10,10 +10,16 @@ ext_modules = [
         Extension("AmpBridge.cscalar.wrapper", sources=["AmpBridge/cscalar/wrapper.pyx"], include_dirs=["."])
         ]
 
+ext_modules_coptimization = [
+        Extension("AmpBridge.coptimization.acc_grad_desc",
+            sources=["AmpBridge/coptimization/acc_grad_desc.pyx"])
+        ]
+
+
 setup(
         name = "cAccelerate",
         cmdclass = {"build_ext" : build_ext},
-        ext_modules = ext_modules,
+        ext_modules = ext_modules_cscalar + ext_modules_coptimization,
         script_args=["build_ext"],
         options={"build_ext" : {"inplace" : True, "force" : True}},
         )
