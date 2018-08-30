b86e1fb604b96c6aaeaae057e810b87fb97075fa
Shuaiwen Wang <sw2853@columbia.edu>
2018-08-30 16:27:11 -0400

    updated __init__

--- AmpBridge/__init__.py
+++ AmpBridge/__init__.py
@@ -1,6 +1,9 @@
-from .AMPtheory import *
-from .lib import *
-from .linear_model import *
+# from .lib import *
+# from .linear_model import *
 from . import cscalar
 from . import coptimization
+from .gaussian_tuning import *
+from .amp_se import *
+from .mse_expand import *
+from .two_stage import *
 
