4c99f32bc4c56d2bc83f123d5a7cd21375e339d2
Shuaiwen Wang <sw2853@columbia.edu>
2018-08-30 22:32:47 -0400

    removed clib, update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 ext_modules_cscalar = [
-        Extension("AmpBridge.cscalar.clib", sources=["AmpBridge/cscalar/clib.pyx"], libraries=["m"]),
         Extension("AmpBridge.cscalar.gaussian", sources=["AmpBridge/cscalar/gaussian.pyx"], libraries=["m"]),
         Extension("AmpBridge.cscalar.proximal", sources=["AmpBridge/cscalar/proximal.pyx"], libraries=["m"]),
         Extension("AmpBridge.cscalar.amp_mse", sources=["AmpBridge/cscalar/amp_mse.pyx"], libraries=["m"], include_dirs=["."]),
@@ -11,8 +10,8 @@ ext_modules_cscalar = [
         ]
 
 ext_modules_coptimization = [
-        Extension("AmpBridge.coptimization.acc_grad_desc",
-            sources=["AmpBridge/coptimization/acc_grad_desc.pyx"])
+        Extension("AmpBridge.coptimization.bridge_coord_desc",
+            sources=["AmpBridge/coptimization/bridge_coord_desc.pyx"])
         ]
 
 
