b5e25834aee8aefb0784a95169db32d08b0d0761
Shuaiwen Wang <sw2853@columbia.edu>
2018-08-30 22:38:04 -0400

    modify __iint__

--- AmpBridge/__init__.py
+++ AmpBridge/__init__.py
@@ -1,6 +1,6 @@
 # from .lib import *
 # from .linear_model import *
-from . import cscalar
+from .cscalar import *
 from .coptimization import bridge_Lq, vec_bridge_Lq
 from .gaussian_tuning import *
 from .amp_se import *
