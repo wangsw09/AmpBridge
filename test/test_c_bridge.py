8a3d165aa1f2e5fe9be6ee3f03a73c1831d1026f
Shuaiwen Wang <sw2853@columbia.edu>
2018-08-31 23:40:51 -0400

    modify the test case for bridge

--- test/test_c_bridge.py
+++ test/test_c_bridge.py
@@ -2,19 +2,68 @@ import sys
 sys.path.insert(0, "/home/wangsw09/work/Proj-AmpBridge/")
 
 import numpy as np
+import numpy.random as npr
+from sklearn.linear_model import Lasso, Ridge
 
 from AmpBridge import bridge_Lq, vec_bridge_Lq
 
-TOL = 1e-8
+TOL = 1e-7
 
-def test_bridge_Lq():
-    tol = 1e-10
-    X = np.array([[1, 1], [1, 3], [2, 1]], dtype=np.float64)
-    beta = np.array([1, 0], dtype=np.float64)
-    y = np.array([1.5, 0, 3], dtype=np.float64)
+def test_bridge_L1():
+    tol = 1e-12
+    n = 1000
+    p = 200
+    X = npr.normal(size=(n, p))
+    beta = npr.normal(size=p)
+    y = np.dot(X, beta) + 0.5 * npr.normal(size=n)
     lam = 1.0
 
-    beta_true = np.array([385.0 / 300.0, -0.2], dtype=np.float64)
-    beta_hat = bridge_Lq(X, y, lam, 1, abs_tol=tol, iter_max=10000)
-    assert np.abs(beta_hat - beta_true).max() < TOL
+    las = Lasso(alpha=lam / n, fit_intercept=False, tol=tol, max_iter=50000)
+    las.fit(X, y)
+    beta_skl = las.coef_
+    beta_hat = bridge_Lq(X, y, lam, 1, abs_tol=tol, iter_max=50000)
+    assert np.abs(beta_hat - beta_skl).max() < TOL
+    
+    tol = 1e-12
+    n = 500
+    p = 700
+    X = npr.normal(size=(n, p))
+    beta = npr.normal(size=p)
+    y = np.dot(X, beta) + 0.5 * npr.normal(size=n)
+    lam = 1.0
+
+    las = Lasso(alpha=lam / n, fit_intercept=False, tol=tol, max_iter=50000)
+    las.fit(X, y)
+    beta_skl = las.coef_
+    beta_hat = bridge_Lq(X, y, lam, 1, abs_tol=tol, iter_max=50000)
+    assert np.abs(beta_hat - beta_skl).max() < TOL
+
+def test_bridge_L2():
+    tol = 1e-10
+    n = 1000
+    p = 200
+    X = npr.normal(size=(n, p))
+    beta = npr.normal(size=p)
+    y = np.dot(X, beta) + 0.5 * npr.normal(size=n)
+    lam = 1.0
+
+    rig = Ridge(alpha=lam * 2, fit_intercept=False, tol=tol, max_iter=10000)
+    rig.fit(X, y)
+    beta_skl = rig.coef_
+    beta_hat = bridge_Lq(X, y, lam, 2, abs_tol=tol, iter_max=10000)
+    assert np.abs(beta_hat - beta_skl).max() < TOL
+    
+    tol = 1e-10
+    n = 800
+    p = 1000
+    X = npr.normal(size=(n, p))
+    beta = npr.normal(size=p)
+    y = np.dot(X, beta) + 0.5 * npr.normal(size=n)
+    lam = 1.0
+
+    rig = Ridge(alpha=lam * 2, fit_intercept=False, tol=tol, max_iter=10000)
+    rig.fit(X, y)
+    beta_skl = rig.coef_
+    beta_hat = bridge_Lq(X, y, lam, 2, abs_tol=tol, iter_max=10000)
+    assert np.abs(beta_hat - beta_skl).max() < TOL
 
