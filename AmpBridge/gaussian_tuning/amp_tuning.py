5c2f112675a8283694107cfd9da5e1c469343ca6
Shuaiwen Wang <sw2853@columbia.edu>
2019-09-18 00:21:06 -0700

    Starting to work on (recover) the tuning module.

---
+++ AmpBridge/gaussian_tuning/amp_tuning.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def backTrackSearch(X, y, delta, init_lam_range):
+    """
+    This function aims to implement the optimal tuning process of Bridge regression through the back-tracking search mechanism.
+    """
+    pass
